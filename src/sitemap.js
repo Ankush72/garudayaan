f5b52e2572a2a816f9a5412ba83e54feab06c9b0
Ankush kumar <69650280+Ankush8950@users.noreply.github.com>
2023-08-24 15:08:56 +0530

    update sitemap

--- src/sitemap.js
+++ src/sitemap.js
@@ -4,15 +4,15 @@ const hostname = 'https://www.garudayaan.com';
 
 const urls = [
   { url: '/', changefreq: 'daily', priority: 1 },
-  { url: '/about-us', changefreq: 'monthly', priority: 0.8 },
-  { url: '/contact', changefreq: 'monthly', priority: 0.8 },
-  { url: '/our-service', changefreq: 'monthly', priority: 0.8 },
-  { url: '/our-service/parcel-delivery', changefreq: 'monthly', priority: 0.8 },
-  { url: '/our-service/container-load-service', changefreq: 'monthly', priority: 0.8 },
-  { url: '/our-service/road-freight-delivery', changefreq: 'monthly', priority: 0.8 },
-  { url: '/privacy-policy', changefreq: 'monthly', priority: 0.8 },
-  { url: '/terms-conditions', changefreq: 'monthly', priority: 0.8 },
-  { url: '/driver-terms-condition', changefreq: 'monthly', priority: 0.8 },
+  { url: '/about-us', changefreq: 'daily', priority: 0.8 },
+  { url: '/contact', changefreq: 'daily', priority: 0.8 },
+  { url: '/our-service', changefreq: 'daily', priority: 0.8 },
+  { url: '/our-service/parcel-delivery', changefreq: 'daily', priority: 0.8 },
+  { url: '/our-service/container-load-service', changefreq: 'daily', priority: 0.8 },
+  { url: '/our-service/road-freight-delivery', changefreq: 'daily', priority: 0.8 },
+  { url: '/privacy-policy', changefreq: 'daily', priority: 0.8 },
+  { url: '/terms-conditions', changefreq: 'daily', priority: 0.8 },
+  { url: '/driver-terms-condition', changefreq: 'daily', priority: 0.8 },
 
   // Add additional pages here
 ];
